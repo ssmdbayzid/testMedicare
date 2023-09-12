7737556a228e6dd6808c6c64c602c9fbe2ee5aaf
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-09-12 23:21:56 +0600

    doctor details page

---
+++ src/Pages/Doctor/DoctorAbout.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const DoctorAbout = () => {
+  return (
+    <div>
+        <div>
+            <h3 className="text-[20px] leading-[30px] text-textColor font-semibold flex items-center  gap-2">
+                About of <span className="text-irisBlueColor font-bold text-[24px] leading-9"> S S Md. Bayzid</span>
+            </h3>
+            <p className="text_para">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum ratione quo perferendis numquam ut vitae asperiores exercitationem magni, deserunt soluta assumenda expedita consequatur amet eum adipisci nostrum architecto? Facilis, laboriosam!
+            </p>
+        </div>
+        <div className="mt-12">
+            <h3 className="text-[20px] leading-7 text-headingColor font-semibold">
+                Education
+            </h3>
+
+            <ul className="pt-4 md:p-5">
+            <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px] ">
+                <div className="">
+                    <span className="text-irisBlueColor  text-[15px] leading-6 font-semibold">
+                        23 June, 2008
+                    </span>
+                    <p className="text-[16px] leading-6 font-medium text-textColor">
+                        PHD in Surgeon
+                    </p>
+                </div>
+                <p className="text-[14px] leading-5 font-medium text-textColor">
+                    New Apollo Hospital, New York
+                </p>
+            </li>
+            <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px] ">
+                <div className="">
+                    <span className="text-irisBlueColor  text-[15px] leading-6 font-semibold">
+                        23 June, 2008
+                    </span>
+                    <p className="text-[16px] leading-6 font-medium text-textColor">
+                        PHD in Surgeon
+                    </p>
+                </div>
+                <p className="text-[14px] leading-5 font-medium text-textColor">
+                    New Apollo Hospital, New York
+                </p>
+            </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default DoctorAbout
--- src/Pages/Doctor/DoctorDetails.jsx
+++ src/Pages/Doctor/DoctorDetails.jsx
@@ -1,20 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import avatar from '../../assets/images/avatar-icon.png'
+import star from '../../assets/images/Star.png'
+import DoctorAbout from './DoctorAbout'
+import Feedback from './Feedback'
 
 const DoctorDetails = () => {
-  return (<section>
-      <div className="max-w-[11170px] px-5 mx-auto">
+    const [tab, setTab] = useState("about")
+
+
+  return (<section className='section'>
+      <div className="max-w-[1170px] px-5 mx-auto">
         <div className="grid md:grid-cols-3 gap-12">
           <div className="md:col-span-2">
             <div className="flex items-center gap-5">
-              <figure className="max-w-[200px] max-h-[200px]">
-                <img src="" alt="" className="w-full" />
+              <figure className="max-w-[200px] max-h-[200px] overflow-hidden">
+                <img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" className="w-full object-cover" />
               </figure>
-            </div>
-            <div>
+              <div>
               <span className="bg-[#CCF0F3] text-irisBlueColor py-1 lg:py-2 px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
                 Sergeon
               </span>
+              <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
+                S S Md. Bayzid
+              </h3>
+              <div className="flex items-center gap-2">
+                <img src={star} alt="" className="w-6 h-6" />
+                <span className="font-semibold">4.8</span>
+                (292)
+              </div>
+              <p className="mt-3 text-_para text-[14px] md:text-15 leading-6 lg:max-w-[390px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos minima ut laboriosam porro tenetur mollitia, </p>
             </div>
+            </div>
+
+            <div className="mt-12 border-b border-solid border-[#0066dd34]">
+            <button 
+              onClick={()=>setTab("about")}
+            className={`${tab == "about" && "font-bold border-b-2 border-solid"} py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor `}>
+              About
+            </button>
+
+            <button 
+              onClick={()=>setTab("feedback")}
+              className={`${tab == "feedback" && "font-bold border-b-2 border-solid"} py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor `}>
+              Feedback
+            </button>
+            </div>
+            <div className="mt-12">
+              {
+                tab === "about" && <DoctorAbout />
+              }
+              {
+                tab === "feedback" && <Feedback />
+              }
+            </div>
+          
           </div>
         </div>
       </div>
