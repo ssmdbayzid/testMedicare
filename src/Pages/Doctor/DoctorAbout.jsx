135328e766a5954b9cb9ddf672de859911d47e14
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-09-13 20:00:46 +0600

    Doctor About complete

--- src/Pages/Doctor/DoctorAbout.jsx
+++ src/Pages/Doctor/DoctorAbout.jsx
@@ -9,7 +9,7 @@ const DoctorAbout = () => {
             <h3 className="text-[20px] leading-[30px] text-textColor font-semibold flex items-center  gap-2">
                 About of <span className="text-irisBlueColor font-bold text-[24px] leading-9"> S S Md. Bayzid</span>
             </h3>
-            <p className="text_para">
+            <p className="text_para text-justify">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum ratione quo perferendis numquam ut vitae asperiores exercitationem magni, deserunt soluta assumenda expedita consequatur amet eum adipisci nostrum architecto? Facilis, laboriosam!
             </p>
         </div>
@@ -22,7 +22,7 @@ const DoctorAbout = () => {
             <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px] ">
                 <div className="">
                     <span className="text-irisBlueColor  text-[15px] leading-6 font-semibold">
-                        23 June, 2008
+                    {formateDate("09-13-2014")} - {formateDate("09-12-2016")}
                     </span>
                     <p className="text-[16px] leading-6 font-medium text-textColor">
                         PHD in Surgeon
@@ -35,7 +35,7 @@ const DoctorAbout = () => {
             <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px] ">
                 <div className="">
                     <span className="text-irisBlueColor  text-[15px] leading-6 font-semibold">
-                        {formateDate("12-04-2010")}
+                    {formateDate("07-04-2010")} - {formateDate("09-02-2014")}
                     </span>
                     <p className="text-[16px] leading-6 font-medium text-textColor">
                         PHD in Surgeon
@@ -45,6 +45,31 @@ const DoctorAbout = () => {
                     New Apollo Hospital, New York
                 </p>
             </li>
+            </ul>
+        </div>
+        <div className="mt-12">
+        <h3 className="text-[20px] leading-7 text-headingColor font-semibold">
+                Experience
+            </h3>
+            <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+                <li className="p-4 rounded bg-[#fff9ea]">
+                    <span className="text-secondaryColor text-[15px] leading-[25px] font-semibold">                    
+                    {formateDate("08-12-2010")} - {formateDate("08-12-2014")}
+                    </span>
+                    <p className="text-[16px] mt-3 leading-6 semibold">
+                        Sr. Sergion
+                    </p>
+                    <p className="text-sm">New Apollo Hospital, New  York</p>
+                </li>
+                <li className="p-4 rounded bg-[#fff9ea]">
+                    <span className="text-secondaryColor text-[15px] leading-[25px] font-semibold">                    
+                    {formateDate("08-12-2010")} - {formateDate("08-12-2014")}
+                    </span>
+                    <p className="text-[16px] mt-3 leading-6 semibold">
+                        Sr. Sergion
+                    </p>
+                    <p className="text-sm">New Apollo Hospital, New  York</p>
+                </li>
             </ul>
         </div>
     </div>
--- src/Pages/Doctor/Feedback.jsx
+++ src/Pages/Doctor/Feedback.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import avatar from '../../assets/images/avatar-icon.png'
+import { formateDate } from '../../utils/formateDate'
 
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+    <div>
+      <div className="mb-12">
+        <p className="text-lg leading-7 mb-7 font-semibold text-textColor">
+          All reviews (272)
+        </p>
+
+        <div className="flex items-center gap-5">
+          <figure  className="h-10 w-10 rounded-full">
+            <img src={avatar} alt="" className="w-full" />
+          </figure>
+          <div className="">
+            <h3 className="font-bold text-[18px] leading-7 text-primaryColor">Ali Ahmed</h3>
+            <p className="text-[14px]"> {formateDate("06-08-2023")} </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
