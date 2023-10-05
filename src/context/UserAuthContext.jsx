516120f74f2f9e2b6afadcbcfa7f4003a516f7a5
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-10-05 09:06:27 +0600

    firebase add

--- src/context/UserAuthContext.jsx
+++ src/context/UserAuthContext.jsx
@@ -1,20 +1,45 @@
-import React, { createContext } from 'react'
+
+import React, { createContext, useEffect } from 'react'
 
 const userAuthContext = createContext();
-import { useCreateUserWithEmailAndPassword, useSign } from 'react-firebase-hooks/auth';
+import {
+  useCreateUserWithEmailAndPassword,
+  useSignWithUserAndPassword,
+  useAuthState,
+  useSignOut, useContext } from 'react-firebase-hooks/auth';
 
 export const  UserAuthContextProvider =({children}) =>{
+
+  const [user, setUser] = useState("")
+
+ const signUp = (email, password) =>{
+    return useCreateUserWithEmailAndPassword(auth, email, password)
+  }
+  // Login
+  const Login = (email, password) =>{
+    return useSignWithUserAndPassword(auth, email, password)
+  }
+  // Sign Out
+  const signout = (email, password) =>{
+    return useSignOut(auth, email, password)
+  }
+
+  useEffect(()=> {
+    const unsubscribe = useAuthState(auth, (currentUser)=>{
+      setUser(currentUser)
+    });
+
+    return ()=> {
+      unsubscribe()
+    }
+  },[])
+
+
     return <UserAuthContext.Provider>{children}</UserAuthContext.Provider>
 }
+
+
 
 export const useUserAuth = () =>{
     return useContext(userAuthContext)
 }
-
-const UserAuthContext = () => {
-  return (
-    <div>UserAuthContext</div>
-  )
-}
-
-export default UserAuthContext
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAY1bYD96t4Z8DEexkMa6fhZc9EQ6C2ZyY",
+  authDomain: "medicare-817a0.firebaseapp.com",
+  projectId: "medicare-817a0",
+  storageBucket: "medicare-817a0.appspot.com",
+  messagingSenderId: "381192860579",
+  appId: "1:381192860579:web:f90fc3dfca1a176b81e016"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
