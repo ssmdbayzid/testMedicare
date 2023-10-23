dca5543b120bc3199effa1ceef0fa3f9bb42ab86
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-10-23 22:04:31 +0600

    add theme toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
+  
+  daisyui : {
+    themes: [
+      {
+      light: {
         primaryColor: '#0067FF',
         secondaryColor: '#FFB60D',
         purpleColor: '#9771FF',
@@ -14,7 +18,20 @@ module.exports = {
         textColor: '#4E545F',
       },
       
-    },
+      dark: {
+        "primary": "#D9F99D",      
+          "secondary": "#FDE68A",        
+          "accent": "#4B5563",        
+          "neutral": "#F3F4F6",        
+          "base-100": "#1F2937",        
+          "info": "#98A8DD",        
+          "success": "#1BBB70",        
+          "warning": "#F59E0B",        
+          "error": "#FB7185",
+      }
+    }
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  // plugins: [],
 }
