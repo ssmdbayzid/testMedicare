f70ff101d93ce54bbe462714ffe68b4a002de0ab
ssmdbayzid <ssmdbayzid@gmail.com>
2024-01-26 10:08:16 +0600

    update menubar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -115,7 +115,7 @@ const Header = () => {
         </div>
       </div>
       
-          <div className={`md:hidden  bg-white shadow-xl w-full max-w-[400px]  ${open ? "left-0" : "-z-20 -left-full"} top-[70px] absolute transition-all  duration-500 w-[75%] shadow-black shadow-xl`}>
+          <div className={`md:hidden  bg-white shadow-xl w-full max-w-[400px]  ${open ? "left-0" : "-z-20 -left-full"} top-0 absolute transition-all  duration-500 w-[75%] shadow-black shadow-xl`}>
             <Menu navLinks={navLinks} setOpen={setOpen}  className="relative "/>
           </div>
         
--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../assets/images/logo.png'
+
 import { Link, NavLink } from 'react-router-dom'
 import { IoHomeOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
@@ -7,7 +7,8 @@ import { FaLaptopMedical } from "react-icons/fa6";
 import { CgProfile } from "react-icons/cg";
 import { RiLogoutBoxLine } from "react-icons/ri";
 import { useSelector } from 'react-redux';
-import { logOut, selectCurrentUser } from 'features/auth/authSlice';
+import { logOut, selectCurrentRole, selectCurrentUser } from 'features/auth/authSlice';
+import logo from '../assets/images/pluslogo.png'
 
   
 const navLinks = [
@@ -15,12 +16,7 @@ const navLinks = [
       path: '/home',
       display: "Home",
       icon: <IoHomeOutline />
-  },
-  {
-      path: '/profile',
-      display: "Profile",
-      icon: <CgProfile />
-  },
+  },  
   {
       path: '/doctors',
       display: "Doctors",
@@ -40,28 +36,50 @@ const navLinks = [
 
 const Menu = ({setOpen,  open}) => {
   const user =  useSelector(selectCurrentUser)
+  const role =  useSelector(selectCurrentRole)
 
   
   return ( <>
 
-     <div className='h-[87vh] relative'>
-            <ul className=" flex flex-col justify-center  mx-5 ml-4 pt-5">
+     <div className='h-[100svh] relative'>
+     <a href="/home" className="flex ml-5 pt-10 text-2xl md:text-3xl items-center text-[var(--heading-color)] gap-3 font-extrabold">
+                <img src={logo} alt="Logo" className="cursor-pointer  w-6 md:w-8"/>
+                <p>Mediplus</p>
+                </a>
+            <ul className="gap-6  flex flex-col justify-center  mx-5 ml-4 pt-12">
                     { navLinks && navLinks.map((link, index)=>
-                    <NavLink to={link.path} className={navClass => navClass.isActive ? "text-[var(--primary-color)] text-lg  font-[600] text-center  flex items-center gap-12 py-3 bg-blue-200"
-                    : "text-lg text-textColor font-[500]  text-center flex items-center gap-12 hover:bg-blue-200 py-3 hover:blue-200" } key={index}>
+                    <NavLink to={link.path} onClick={()=>setOpen(open)} className={navClass => navClass.isActive ? "text-[var(--primary-color)] text-lg  font-[600] text-center  flex items-center gap-12 py-3 px-2 bg-blue-200"
+                    : "px-2 text-lg text-textColor font-[500]  text-center flex items-center gap-12 hover:bg-blue-200 py-3 hover:blue-200" } key={index}>
                       <span>{link.icon}</span>
-                      {link.display}
+                      <span>{link.display}</span>
+                      
                       </NavLink>)
                     }
                 </ul>
-                <Link to={"/login"}><button className='w-[90%] text-white text-xl bg-blue-700 py-2  ml-3 mr-3 rounded-full mt-3'>Login</button></Link>
-                <Link to={"/signup"}><button className='w-[90%] text-white text-xl bg-gray-600 py-2  ml-3 mr-3 rounded-full mt-3'>Signup</button></Link>
+                {user && role ? 
+                <>
+
+                <NavLink  
+                onClick={()=>setOpen(open)}
+                to={`${role == "patient" ? "/users" : "/doctors" }/profile/${user._id}`} className={navClass => navClass.isActive ? "text-[var(--primary-color)] text-lg  font-[600] text-center  flex items-center gap-2 py-3 px-2 bg-blue-200 mx-5"
+                    : "px-2 text-lg text-textColor font-[500]  text-center flex items-center gap-12 hover:bg-blue-200 py-3 hover:blue-200 mx-5" } >
+                      <span><CgProfile /></span>
+                      Profile
+                      </NavLink>
+
+                <Link
+
+                 onClick={()=>{logOut(); setOpen(open)}}
+                to={"/login"}><button className='w-[90%] text-white text-xl bg-blue-700 py-2  ml-3 mr-3 rounded-full mt-10'>Log Out</button></Link> </>
+
+                :  <> <Link to={"/login"}><button className='w-[90%] text-white text-xl bg-blue-700 py-2  ml-3 mr-3 rounded-full mt-3'>Login</button></Link>
+                <Link to={"/signup"}><button className='w-[90%] text-white text-xl bg-gray-600 py-2  ml-3 mr-3 rounded-full mt-3'>Signup</button></Link> </>}
       
       </div>
 
       {/* ------------- User Email & Logout ----------- */}
-    {user &&  <div className='absolute left-0 bottom-0 pl-2 bg-black bg-opacity-50 bg-transparent w-full flex items-center justify-between'>
-        <div className='flex'>
+    {user &&  <div className='absolute left-0 bottom-0 pl-2 bg-slate-500  w-full flex items-center justify-between'>
+        <div className='flex '>
         <img src={user.photo} alt="" className='w-10 rounded-md mr-3' />
         <div>
           <p className='text-white text-lg'>{user.name}</p>
@@ -70,7 +88,7 @@ const Menu = ({setOpen,  open}) => {
         </div>
         
         <p
-        onClick={()=>logOut()}
+        onClick={()=>{logOut(); setOpen(open)}}
         className='bg-red-600 px-3 py-6 h-full cursor-pointer '>
 
         < RiLogoutBoxLine className='text-2xl text-white' />
